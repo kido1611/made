eee6c1bb1b2e773946a40c941ecdce537a12abf8
Muhammad Abdusy Syukur <ahmadci3@gmail.com>
2019-11-07 05:24:11 +0700

    Update 4 final project

--- app/src/main/java/id/kido1611/dicoding/moviecatalogue3/widget/StackRemoteViewsFactory.kt
+++ app/src/main/java/id/kido1611/dicoding/moviecatalogue3/widget/StackRemoteViewsFactory.kt
@@ -2,20 +2,18 @@ package id.kido1611.dicoding.moviecatalogue3.widget
 
 import android.content.Context
 import android.content.Intent
-import android.database.Cursor
 import android.os.Binder
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import androidx.core.os.bundleOf
 import com.bumptech.glide.Glide
 import id.kido1611.dicoding.moviecatalogue3.R
-import id.kido1611.dicoding.moviecatalogue3.db.FavoriteMovieProvider
 import id.kido1611.dicoding.moviecatalogue3.db.MovieDatabase
 import id.kido1611.dicoding.moviecatalogue3.model.Movie
 
 internal class StackRemoteViewsFactory(private val context: Context): RemoteViewsService.RemoteViewsFactory {
     private var movieItems = ArrayList<Movie>()
-    private var cursor: Cursor? = null
+
     private lateinit var movieDatabase: MovieDatabase
 
     override fun onCreate() {
@@ -27,42 +25,13 @@ internal class StackRemoteViewsFactory(private val context: Context): RemoteView
     override fun getItemId(position: Int): Long = 0
 
     override fun onDataSetChanged() {
-        if(cursor != null){
-            cursor?.close()
-        }
-
         movieItems.clear()
         val identityToken = Binder.clearCallingIdentity()
-        cursor = context.contentResolver.query(
-            FavoriteMovieProvider.CONTENT_URI,
-            null,
-            null,
-            null,
-            null
-        )
-        val result = convertCursorToList(cursor!!)
+
+        val result = movieDatabase.movieDAO().getAllMovies()
         movieItems.addAll(result)
 
         Binder.restoreCallingIdentity(identityToken)
-    }
-
-    private fun convertCursorToList(cur: Cursor) : ArrayList<Movie>{
-        val result = ArrayList<Movie>()
-        while(cur.moveToNext()){
-            val data = Movie(
-                id = cur.getInt(cur.getColumnIndexOrThrow("id")),
-                poster = cur.getString(cur.getColumnIndexOrThrow("poster")),
-                backdrop = cur.getString(cur.getColumnIndexOrThrow("backdrop")),
-                title = cur.getString(cur.getColumnIndexOrThrow("title")),
-                overview = cur.getString(cur.getColumnIndexOrThrow("overview")),
-                releaseDate = cur.getString(cur.getColumnIndexOrThrow("releaseDate")),
-                voteAverage = cur.getDouble(cur.getColumnIndexOrThrow("vote")),
-                runtime = cur.getInt(cur.getColumnIndexOrThrow("runtime"))
-            )
-            result.add(data)
-        }
-
-        return result
     }
 
     override fun hasStableIds(): Boolean = false
@@ -97,6 +66,6 @@ internal class StackRemoteViewsFactory(private val context: Context): RemoteView
     override fun getViewTypeCount(): Int = 1
 
     override fun onDestroy() {
-
+        
     }
 }
